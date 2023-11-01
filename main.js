41644cafc2f470f831d5e60fc27acde40d88189b
0Naclara <119817152+0Naclara@users.noreply.github.com>
2023-11-01 11:34:23 -0300

    upload dos arquivos do site

---
+++ main.js
@@ -0,0 +1,8 @@
+let ul = document.querySelector('nav .ul');
+
+function openMenu(){
+    ul.classList.add('open'); //add em um determinado elemento
+}
+function closeMenu(){
+    ul.classList.remove('open');
+}
